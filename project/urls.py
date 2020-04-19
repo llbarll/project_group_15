4e9d5ccef0675c752f4e2a1484a4ef41b1bb3efc
llbarll <45006662+llbarll@users.noreply.github.com>
2020-04-19 18:19:08 +0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from homepage.views import homepage
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
     path('users/', include('users.urls')),
+    url('appointment_files/', include('appointments.urls')),
 
 ]
